a737c4a92a2e815d6324e1826c6f83e154a73293
Christian Rasmussen <himrasmussen@gmail.com>
2016-01-24 23:43:27 -0800

    Simplified

--- BinaryMachine.py
+++ BinaryMachine.py
@@ -5,18 +5,13 @@ Chra94
 
 
 def binary():
-    # binary = input('Enter a binary number')
-    binary = '10100100111010100'
+    binary = input('Enter a binary number')
     n = -1
     s = 0
     for i in binary[::-1]:
         n += 1
         if i == '1':
-            if n != 0:
-                s += 2**n
-            else:
-                s += 1
-
+            s += 2**n
     print(s)
 
 def base10():
